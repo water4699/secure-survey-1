73fda7b9853aa3c33f5270deebff62776c298546
water4699 <10322937@qq.com>
2025-11-19 22:43:25 +0800

    Clean repository: Only secure-survey project with complete FHE functionality

--- secure-survey/calc-signature.js
+++
@@ -1,4 +0,0 @@
-const { keccak256, toUtf8Bytes } = require('ethers');
-
-console.log('submitSurvey(uint8):', '0x' + keccak256(toUtf8Bytes('submitSurvey(uint8)')).slice(0, 10));
-console.log('submitSurvey(externalEuint32,bytes):', '0x' + keccak256(toUtf8Bytes('submitSurvey(externalEuint32,bytes)')).slice(0, 10));
